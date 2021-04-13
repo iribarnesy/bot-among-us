d381ab06bc9fdfa187ddbcc34f61b331f66297ce
Kiulou <arnaud.detchenique09@orange.fr>
2021-04-13 17:02:03 +0200

    refactor: navigation - add conversion

--- src/navigation.py
+++ src/navigation.py
@@ -11,10 +11,17 @@ class NavigationManager(metaclass=SingletonMeta):
     def __init__(self, map_img_path):
         self.map_img_path = map_img_path
         self.map_matrix = []
+        self.origine_height = 1080
+        self.origine_width = 1920
+        self.height = 0
+        self.width = 0
 
     def init_matrix(self):
-        img_map_pix = Image.open(self.map_img_path)
+        img_map_pix = Image.open(self.map_img_path)    
+
         img_map = np.array(img_map_pix)
+        self.height = img_map.shape[0]
+        self.width = img_map.shape[1]
 
         self.map_matrix = np.count_nonzero(img_map, axis=2)
         self.map_matrix = np.where(self.map_matrix > 1, 1, 0)
@@ -40,4 +47,21 @@ class NavigationManager(metaclass=SingletonMeta):
             print(grid.grid_str(path=path, start=grid.node(*path[0]), end=grid.node(*path[-1])))
         else:
             print("-- No path possible --")
+
+    def convert_for_realmap(self, position):
+        scale = self.origine_height / self.height
+        print(str(self.origine_height) + " / " + str(self.height))
+        print(scale)
+        pos_x, pos_y = position
+
+        return int(pos_x*scale), int(pos_y*scale)
+
+    def convert_for_minimap(self, position):
+        scale = self.origine_height / self.height
+        pos_x, pos_y = position
+
+        return int(pos_x/scale), int(pos_y/scale)
+
+
+
 
