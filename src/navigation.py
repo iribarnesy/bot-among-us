e9bfc5c3af891a7cc2c05a7becbe9c19b5e5d72e
Florent Denat <denatflore@eisti.eu>
2021-04-13 11:04:51 +0200

    feat : init_matrix

--- src/navigation.py
+++ src/navigation.py
@@ -1,3 +1,7 @@
+from PIL import Image
+import numpy as np
+import copy
+
 from src.utils import SingletonMeta
 
 class NavigationManager(metaclass=SingletonMeta):
@@ -6,7 +10,11 @@ class NavigationManager(metaclass=SingletonMeta):
         self.map_matrix = []
 
     def init_matrix(self):
-        pass
+        img_map_pix = Image.open(self.map_img_path)
+        img_map = np.array(img_map_pix)
+
+        self.map_matrix = np.count_nonzero(img_map, axis=2)
+        self.map_matrix = np.where(self.map_matrix > 1, 1, 0)
 
     def calculate_path(self, source_coordinates, target_coordinates):
         pass
