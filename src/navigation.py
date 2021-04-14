ba52a1f14b920ce49d7c793780a4acb089265228
Sylvain Iribarne <siribarne@ippon.fr>
2021-04-14 11:34:01 +0200

    feat: convert pixels in input and outputs of this class

--- src/navigation.py
+++ src/navigation.py
@@ -13,38 +13,36 @@ class NavigationManager(metaclass=SingletonMeta):
         self.map_matrix = None
         self.original_height = 1080
         self.original_width = 1920
-        self.height = 0
-        self.width = 0
+        self.scale = None
+
+        self.init_matrix()
 
     def init_matrix(self):
         img_map_pix = Image.open(self.map_img_path)    
 
         img_map = np.array(img_map_pix)
-        self.height = img_map.shape[0]
-        self.width = img_map.shape[1]
+        self.scale = self.original_width // img_map.shape[1]
 
         self.map_matrix = np.count_nonzero(img_map, axis=2)
         self.map_matrix = np.where(self.map_matrix > 1, 1, 0)
 
     def calculate_path(self, source_coordinates, target_coordinates):
         """ calculate a path between a start point and an end point of a matrix
-        input : self.map_matrix
-        output : path
+        input : coordinates sized for the realmap
+        output : path with coordinates sized for realmap
         """
-        if self.map_matrix is None:
-            self.init_matrix()
-        
+        source_coordinates, target_coordinates = self.convert_for_minimap(source_coordinates), self.convert_for_minimap(target_coordinates)
         grid = Grid(matrix=self.map_matrix)
         start = grid.node(*source_coordinates)
         end = grid.node(*target_coordinates)
         #finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
         finder = AStarFinder(diagonal_movement=DiagonalMovement.only_when_no_obstacle)
-        path, runs = finder.find_path(start, end, grid)
+        path, _ = finder.find_path(start, end, grid)
 
-        return path, runs
+        return [self.convert_for_realmap(coordinates) for coordinates in path]
 
     def path_to_grid(self, path, must_print=True, result_file_path=None):
-        """ Create a representation of a given path in the map_matrix.
+        """ Create a representation of a given path (sized for realmap) in the map_matrix.
         's', 'e' represents respectively the start and end points. 'x' is the path and '#' is an obstacle
         ex : 
             +---+
@@ -54,6 +52,7 @@ class NavigationManager(metaclass=SingletonMeta):
             +---+
         """
         grid = Grid(matrix=self.map_matrix)
+        path = [self.convert_for_minimap(coordinates) for coordinates in path]
         if(len(path) > 0):
             grid_str = grid.grid_str(path=path, start=grid.node(*path[0]), end=grid.node(*path[-1]))
             if result_file_path:
@@ -68,18 +67,12 @@ class NavigationManager(metaclass=SingletonMeta):
             return None
 
     def convert_for_realmap(self, position):
-        scale = self.original_height / self.height
-        print(str(self.original_height) + " / " + str(self.height))
-        print(scale)
         pos_x, pos_y = position
-
-        return int(pos_x*scale), int(pos_y*scale)
+        return int(pos_x*self.scale), int(pos_y*self.scale)
 
     def convert_for_minimap(self, position):
-        scale = self.original_height / self.height
         pos_x, pos_y = position
-
-        return int(pos_x/scale), int(pos_y/scale)
+        return int(pos_x/self.scale), int(pos_y/self.scale)
 
 
 
