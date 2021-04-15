79c78a42b1227e525c15775c75cc2e888b7788ca
Sylvain Iribarne <siribarne@ippon.fr>
2021-04-15 10:10:37 +0200

    feat: add thread safe to singleton

--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,25 @@
 import pyautogui
+from threading import Lock, Thread
+
 
 class SingletonMeta(type):
     """
-    The Singleton class can be implemented in different ways in Python. Some
-    possible methods include: base class, decorator, metaclass. We will use the
-    metaclass because it is best suited for this purpose.
+    This is a thread-safe implementation of Singleton.
     Source: https://refactoring.guru/fr/design-patterns/singleton/python/example#example-0
     """
 
     _instances = {}
+    _lock: Lock = Lock()
 
     def __call__(cls, *args, **kwargs):
         """
         Possible changes to the value of the `__init__` argument do not affect
         the returned instance.
         """
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
+        with cls._lock:
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
         return cls._instances[cls]
 
 def FOCUS_AMONG_SCREEN():
