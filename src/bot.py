06cc978ba45059761154f03d1d262938d71855a4
garciajuli <garciajuli@eisti.eu>
2021-04-15 10:34:10 +0200

    feat :  add next_task to Bot

--- src/bot.py
+++ src/bot.py
@@ -23,7 +23,6 @@ class MovingAction:
     def __init__(self, direction, distance: int):
         self.direction: str = direction
         self.distance: int = distance
-        self.next_task: Task = None
         # Distances over the diags are longer
         # if self.direction in ['top-right', 'bottom-right', 'bottom-left', 'top-left']:
         #     self.distance = distance * math.sqrt(2)
@@ -39,6 +38,7 @@ class Bot:
         self.name = "Le bot"
         self.game_map = game_map.SkeldMap(map_img_path)
         self.position = Position()
+        self.next_task: Task = None
 
     def menu(self):
         print("What would you like to do?")
