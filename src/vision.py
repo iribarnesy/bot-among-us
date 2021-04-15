9dfa70974a89519266d1d007b77358e3b2236da8
Sylvain Iribarne <siribarne@ippon.fr>
2021-04-16 00:10:26 +0200

    feat: add vision loop with thread

--- src/vision.py
+++ src/vision.py
@@ -1,21 +1,47 @@
 from enum import Enum
+import time
+import pyautogui
 
 from src.enums.pixels import Colors, PixelPositions, PixelRegions
 from src.utils import check_image, check_color, check_red, check_pixel_color
+from src.utils import KillableThread, SingletonMeta
 
 class GamePhase(Enum):
     Game = "game"
     Vote = "vote"
     Lobby = "lobby"
 
-class VisionManager:
-    SECONDS_BETWEEN_EACH_SCREEN = 3
+class VisionManager(metaclass=SingletonMeta):
 
     def __init__(self):
         self.vision_screen = None
+        self.vision_thread = None
         self.game_phase: GamePhase = None
         self._is_impostor = None
         # self.is_sabotage_running = None
+        self.SECONDS_BETWEEN_EACH_SCREEN = 1
+        self.MAX_ITERATIONS_FOR_THREAD = 10
+
+
+    def start_vision_loop(self):
+        self.vision_thread = KillableThread(name="compute_screen", target=self.compute_screen)
+        self.vision_thread.start()
+
+    def compute_screen(self):
+        for _ in range(self.MAX_ITERATIONS_FOR_THREAD):
+            self.vision_screen = pyautogui.screenshot()
+            time.sleep(self.SECONDS_BETWEEN_EACH_SCREEN)
+
+    def is_vision_looping(self):
+        if self.vision_thread is not None:
+            return self.vision_thread.is_alive()
+        else:
+            return False
+
+    def stop_vision_loop(self):
+        self.vision_thread.kill()
+        print("Terminate :", self.vision_thread)
+
 
     def get_game_phase(self):
         is_vote_phase = check_image(r"src\img\skip_vote.PNG")
