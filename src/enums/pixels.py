4f886f90760dbb8ef9b382bff8484a9feb25e966
Sylvain Iribarne <siribarne@ippon.fr>
2021-04-16 16:58:53 +0200

    feat: add text colors

--- src/enums/pixels.py
+++ src/enums/pixels.py
@@ -10,10 +10,21 @@ class Colors(Enum):
     VENT_BTN_ACTIVE = (192, 199, 199)
     CUSTOMIZE_BTN_ACTIVE = (238, 238, 238)
 
+    RED_TEXT = (255, 0, 0)
+    YELLOW_TEXT = (245, 229, 4)
+    WHITE_TEXT = (255, 255, 255)
+
+    @classmethod
+    def get_text_colors(cls):
+        return [cls.RED_TEXT.value, cls.YELLOW_TEXT.value, cls.WHITE_TEXT.value]
+
 class PixelPositions(Enum):
     MAIN_BTN = (1769, 942)
+    OPEN_TASKS_BTN = (0, 290)
+    CLOSE_TASKS_BTN = (777, 290)
 
 class PixelRegions(Enum):
     REPORT_BTN = (1670,630),(1870,830)
     KILL_BTN = (1440,850),(1640,1050)
     CHARACTER_NAME = (900,420),(1020,480)
+    TASKS_TAB = (18,120),(748,1080)
