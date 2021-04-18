238068256cbd8901d55922469e2627ffcaf64086
Sylvain Iribarne <siribarne@ippon.fr>
2021-04-18 15:35:43 +0200

    feat: move MovingAction and navigation methods

--- src/navigation.py
+++ src/navigation.py
@@ -4,11 +4,29 @@ from pathfinding.finder.a_star import AStarFinder
 from PIL import Image
 import numpy as np
 import copy
+from itertools import groupby
 
+from src.position import Directions
 from src.utils import SingletonMeta
 
+class MovingAction:
+    """ Represents a moving action.
+    For example : "Go top for 5 pixels" will be Action(direction="top", distance=5)
+    """
+    def __init__(self, direction, distance: int):
+        self.direction: str = direction
+        self.distance: int = distance
+        # Distances over the diags are longer
+        # if self.direction in ['top-right', 'bottom-right', 'bottom-left', 'top-left']:
+        #     self.distance = distance * math.sqrt(2)
+        # else:
+        #     self.distance = distance
+
+    def __repr__(self):
+        return f"{self.distance:.2f} x {self.direction}"
+
 class NavigationManager(metaclass=SingletonMeta):
-    def __init__(self, map_img_path):
+    def __init__(self, map_img_path=None):
         self.map_img_path = map_img_path
         self.map_matrix = None
         self.original_height = 1080
@@ -88,4 +106,47 @@ class NavigationManager(metaclass=SingletonMeta):
 
 
 
+    def get_vector_direction(self, source_coordinates, target_coordinates):
+        """ Substract the two coordinates to get the direction of the vector
+        Return a vector_direction, for example (1,1) for the top-right direction
+        """
+        return tuple(target_coordinates[axis] - source_coordinates[axis] for axis in range(len(source_coordinates)))
+    
+    def get_action_str(self, vector_direction):
+        scale = self.scale
+        vector_directions_to_actions_str = {
+            (0       , -1*scale): Directions.UP,
+            (1*scale , -1*scale): Directions.RIGHT_UP,
+            (1*scale , 0       ): Directions.RIGHT,
+            (1*scale , 1*scale ): Directions.RIGHT_DOWN,
+            (0       , 1*scale ): Directions.DOWN,
+            (-1*scale, 1*scale ): Directions.LEFT_DOWN,
+            (-1*scale, 0       ): Directions.LEFT,
+            (-1*scale, -1*scale): Directions.LEFT_UP
+        }
+        return vector_directions_to_actions_str[vector_direction]
+
+    def get_moving_actions_from_vector_directions(self, vector_directions):
+        """ Get the moving actions by grouping along the same vector_directions and aggregating by counting
+        """
+        scale = self.scale
+        return [MovingAction(self.get_action_str(vector_direction),sum(scale for _ in group)) for vector_direction, group in groupby(vector_directions)]
+
+    def get_moving_actions_to_destination(self, destination, source_coordinates):
+        """ Calculate the path between the current position and the destination given and 
+        generate a list of moving actions to get to the destination.
+        For example, to go right and then top-right it will be : [MovingAction('right',5), MovingAction('top-right', 8.5)]
+        """
+        path = self.calculate_path(source_coordinates, destination)
+        if len(path) == 0:
+            print("Didn't find a path between source and target ! 🐾")
+            return []
+        vector_directions = []
+        source_coordinates = path[0]
+        for target_coordinates in path[1:]:
+            vector_directions.append(self.get_vector_direction(source_coordinates, target_coordinates))
+            source_coordinates = target_coordinates
+        
+        actions = self.get_moving_actions_from_vector_directions(vector_directions)
+        return actions
 
