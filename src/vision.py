b06ae69c6d35d3653cb4ab56606405dc4975c112
Florent Denat <denatflore@eisti.eu>
2021-05-04 15:04:58 +0200

    feat : Patrol, robust walk and autonom impostor

--- src/vision.py
+++ src/vision.py
@@ -39,7 +39,7 @@ class VisionManager(metaclass=SingletonMeta):
         self.sabotage_running = None
 
         self.SECONDS_BETWEEN_EACH_SCREEN = 1
-        self.MAX_ITERATIONS_FOR_THREAD = 60
+        self.MAX_ITERATIONS_FOR_THREAD = 500
         self.event_handler = EventHandler('gamePhaseChanged', 'btnUseChanged', 'btnReportChanged', 'btnKillChanged',
                                          'btnVentChanged', 'btnSabotageChanged', 'btnAdminChanged', 'btnSecurityChanged', 
                                          'tasksTabChanged', 'sabotageRunningChanged')
@@ -53,18 +53,18 @@ class VisionManager(metaclass=SingletonMeta):
     def compute_screen(self):
         for _ in range(self.MAX_ITERATIONS_FOR_THREAD):
             self.vision_screen = pyautogui.screenshot()
-            self.get_game_phase()
             self.is_btn_report_active()
             self.is_btn_admin_active()
             self.is_btn_security_active()
-            # self.is_btn_use_active()
-            if self.want_to_read_tasks and not self.is_read_tasks_running():
-                self.start_read_tasks()
-                
+            self.is_btn_use_active()
             if self.is_impostor():
                 self.is_btn_kill_active()
                 # self.is_btn_sabotage_active()
                 # self.is_btn_vent_active()
+            if self.want_to_read_tasks and not self.is_read_tasks_running():
+                self.start_read_tasks()
+            self.get_game_phase()
+
             time.sleep(self.SECONDS_BETWEEN_EACH_SCREEN)
 
     def is_vision_looping(self):
